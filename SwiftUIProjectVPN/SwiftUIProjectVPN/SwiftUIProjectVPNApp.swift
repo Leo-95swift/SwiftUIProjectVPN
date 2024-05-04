fc22707abdacf3c9244e3705628e4936dde7f3a9
xach55 <xach55@gmail.com>
2024-05-04 21:00:52 +0400

    initial commit

---
+++ SwiftUIProjectVPN/SwiftUIProjectVPN/SwiftUIProjectVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectVPNApp.swift
+//  SwiftUIProjectVPN
+//
+//  Created by Levon Shaxbazyan on 04.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
